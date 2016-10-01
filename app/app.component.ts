9f46f555df41d8ac880914b6b6a231fa52b73b66
Jo Lowres <jolowres@gmail.com>
2016-10-01 09:01:33 +0100

    added test functions

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
-    import { Component } from '@angular/core';
-    @Component({
-      selector: 'my-app',
-      template: '<h1>My First Angular App</h1>'
-    })
-    export class AppComponent { }
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular App</h1>'
+})
+export class AppComponent { }
