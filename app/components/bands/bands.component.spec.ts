5ec8edb6ba1a4fb0db7a1b80b6fe712ca05a8cad
Jo Lowres <joseph.lowres@theaa.com>
2016-10-14 08:26:25 +0100

    first mock test on component works

---
+++ app/components/bands/bands.component.spec.ts
@@ -0,0 +1,45 @@
+import { TestBed }      from '@angular/core/testing';
+import { Provider } from '@angular/core';
+
+import { BandsComponent } from './bands.component';
+import { BandService } from '../../services/band/band.service';
+import { Band } from '../../models/band';
+
+import { Observable } from 'rxjs/Rx';
+
+class MockBandService extends BandService{
+    constructor() {
+        super(null);
+    }
+    getBands() {
+        console.log('retun fake bands');
+        return Observable.of([
+                new Band(99, 'test band', 'test genre')
+        ]);
+    }
+}
+
+describe('BandsComponent Tests', () => {
+
+    var bandsComponent: BandsComponent;
+    var mockBandService: BandService;
+
+    beforeEach(() => {
+        mockBandService = new MockBandService();
+    });
+
+    beforeEach(() => {
+        bandsComponent = new BandsComponent(mockBandService);
+    });
+
+    //smoke test
+    it('expect 1 to be 1', () => {
+        expect(1).toBe(1);
+    });
+
+    it('should return a band length of one when ngInit is called', () => {
+        bandsComponent.ngOnInit();
+        expect(bandsComponent.bands.length).toBe(1);
+    });
+
+});
