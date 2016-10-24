9b437cfd9d132b2fad9d7828c32b80b320017fe8
Jo Lowres <joseph.lowres@theaa.com>
2016-10-24 08:22:41 +0100

    tests run  with router link stubbed out

--- app/components/nav-bar/nav-bar.component.spec.ts
+++ app/components/nav-bar/nav-bar.component.spec.ts
@@ -3,13 +3,16 @@ import { Provider } from '@angular/core';
 import { By }              from '@angular/platform-browser';
 import { DebugElement }    from '@angular/core';
 import { HttpModule } from '@angular/http';
-
 import { MockBackend } from '@angular/http/testing';
+
 import { NavBarComponent } from './nav-bar.component';
 import { NavBarService } from '../../services/nav-bar/nav-bar.service';
 import { Urls } from '../../constants/urls.constants';
 
 import { Observable } from 'rxjs/Rx';
+
+import {RouterLinkStubDirective} from "../../../testing/router-stubs";
+import {RouterOutletStubComponent} from "../../../testing/router-stubs";
 
 let comp: NavBarComponent;
 let fixture: ComponentFixture<NavBarComponent>;
@@ -19,12 +22,19 @@ describe ('NavBarComponent tests', () => {
 
     beforeEach( async(() => {
         TestBed.configureTestingModule({
-                declarations: [ NavBarComponent ],
-                imports: [ HttpModule],
+                declarations: [
+                    NavBarComponent,
+                    RouterLinkStubDirective,
+                    RouterOutletStubComponent
+                ],
+                imports: [
+                    HttpModule
+                ],
                 providers: [
                     Urls,
                     MockBackend,
-                    NavBarService ]
+                    NavBarService
+                ]
             })
             .compileComponents()
             .then(createComponent);
